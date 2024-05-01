1182a7ddb28184c2c9d03a3ca3f0f847396f837a
hasanravshanovv <ravshanovhasana@gmail.com>
2024-05-01 18:50:28 +0300

    DTA-206: Almost fonish sign up modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 "use client"
 
-console.log("hello")
+document.getElementById("open-modal-btn").addEventListener("click", () => {
+  document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close_moda-btn").addEventListener("click", () => {
+  document.getElementById("my-modal").classList.remove("open")
+})
