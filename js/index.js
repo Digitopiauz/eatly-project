71aece1fea9d9d0fbc41251b89c5628b3a4ff6e1
hasanravshanovv <ravshanovhasana@gmail.com>
2024-05-02 12:38:59 +0300

    DTA-206: Finish login modal

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ document.getElementById("open-modal-btn").addEventListener("click", () => {
 document.getElementById("close_moda-btn").addEventListener("click", () => {
   document.getElementById("my-modal").classList.remove("open")
 })
+
+document.getElementById("open-modal-btn_2").addEventListener("click", () => {
+  document.getElementById("my-modal_2").classList.add("open")
+})
+
+document.getElementById("close_moda-btn_2").addEventListener("click", () => {
+  document.getElementById("my-modal_2").classList.remove("open")
+})
